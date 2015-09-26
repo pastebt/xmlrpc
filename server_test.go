3b6f4f665b67990f1536e91a64e1c8a5d2a55a16
pastebt <maye1968@gmail.com>
2015-09-25 17:10:26 -0700

    add more testcase

--- server_test.go
+++ server_test.go
@@ -3,6 +3,8 @@ package xmlrpc
 import (
     "testing"
     "bytes"
+    "net/http"
+    "net/http/httptest"
 )
 
 
@@ -22,7 +24,64 @@ func TestServer(tst *testing.T) {
 }
 
 
+func TestSetLog(tst *testing.T) {
+    h := NewHandler()
+    h.SetLogf(func(r *http.Request, l int, n string) {})
+}
+
+
+type A struct {
+    http.Request
+    i int
+}
+//func (a *A)Add(b int) int { return int(*a) + b }
+func (a *A)Add(b int) int { return a.i + b }
+func (a *A)Del(b int) int { return a.i - b }
+
+func TestRegister(tst *testing.T) {
+    h := NewHandler()
+    h.Register(func (){}, func(n string)string{return n}, true)
+    h.Register(new(A), func(n string)string{if n == "Del" {return ""}; return n}, true)
+    h.Register(new(A), nil, true)
+}
+
+
 func TestWriteFault(tst *testing.T) {
     w := bytes.NewBufferString("")
     writeFault(w, 123, "fault msg string")
+}
+
+
+func TestServeHTTP(tst *testing.T) {
+    h := NewHandler()
+    h.SetLogf(func(r *http.Request, l int, n string) {})
+    // bad xml format
+    buf := bytes.NewBufferString("")
+    buf.Write([]byte(`<?xml version="1.0"?><ethodResponse`))
+    req, err := http.NewRequest("GET", "/rpc", buf)
+    if err != nil {
+        tst.Error(err)
+    }
+    w := httptest.NewRecorder()
+    h.ServeHTTP(w, req)
+    b := w.Body.String()
+    tst.Logf("code=%d, body=%s", w.Code, b)
+
+    buf = bytes.NewBufferString("")
+    //err := xmlrpc.Marshal(buf, "update458", "rule458.txt")
+    err = Marshal(buf, "funcName", "data")
+    if err != nil {
+        tst.Error(err)
+    }
+    req, err = http.NewRequest("GET", "/rpc", buf)
+    if err != nil {
+        tst.Error(err)
+    }
+    w = httptest.NewRecorder()
+
+    h.ServeHTTP(w, req)
+
+    b = w.Body.String()
+    tst.Logf("code=%d, body=%s", w.Code, b)
+
 }
